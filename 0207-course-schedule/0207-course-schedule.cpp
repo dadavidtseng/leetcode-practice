086065fc6f0c182b5312681e00116e2d1137b767
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-07-04 22:37:36 -0500

    Time: 7 ms (45.52%), Space: 19.3 MB (55.35%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,65 +1,42 @@
 class Solution {
-   public:
+public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // Create adjacency list
-        // Create the state for every nodes
-        // 0=unvisited, 1=in-progress, 2=done
         vector<vector<int>> node(numCourses);
-        vector state(numCourses, 0);
+        vector<int> inDegree(numCourses, 0);
 
-        // Iterate through prerequisites to build the graph
-        // node(course -> prereq)
         for (int i = 0; i < static_cast<int>(prerequisites.size()); ++i) {
             int const course = prerequisites[i][0];
             int const prereq = prerequisites[i][1];
 
-            node[course].push_back(prereq);
+            node[prereq].push_back(course);
+            inDegree[course]++;
         }
 
-        // Iterate through every courses,
-        // return false if any cycle is found
-        // Note that we're using numCourses here instead of prerequisites
-        // because the prerequisites.size() might be greater or equal than numCourses
+        queue<int> q;
+
         for (int i = 0; i < numCourses; ++i) {
-            if (dfs(node, state, i)) {
-                return false;
+            if (inDegree[i] == 0) {
+                q.push(i);
             }
         }
 
-        // Return true if no cycle was found
-        return true;
-    }
+        int count = 0;
 
-    //------------------------------------------------------------------------------------------------
-    bool dfs(vector<vector<int>> const& node, vector<int>& state, int i) {
-        // Return true if this node is still in-progress,
-        // means that we've found a cycle
-        if (state[i] == 1) {
-            return true;
-        }
+        while (!q.empty()) {
+            const int i = q.front();
+            q.pop();
 
-        // Return false if this node is done,
-        // means that we're safe to take it
-        if (state[i] == 2) {
-            return false;
-        }
+            count += 1;
 
-        // Mark this node's state as in-progress since it's unvisited
-        state[i] = 1;
-
-        // Iterate through this course(node)'s every prerequisites(neighbor nodes)
-        // return true if any cycle was found
-        for (int j = 0; j < static_cast<int>(node[i].size()); ++j) {
-            int neighbor = node[i][j];
-
-            if (dfs(node, state, neighbor)) {
-                return true;
+            for (int j = 0; j < (int)node[i].size(); ++j) {
+                int neighbor = node[i][j];
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
+                    q.push(neighbor);
+                }
             }
         }
 
-        // Mark this course(node)'s state as completed since we didn't find any cycle
-        state[i] = 2;
-
-        return false;
+        return count == numCourses;
     }
 };
