20f2ea4b1a9e91b3406598b5bac3038c5ceef8ac
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-07-09 17:20:59 -0500

    Time: 0 ms (100%), Space: 22.2 MB (8.46%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -13,13 +13,44 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
+        // Return 0 if the root is nullptr
         if (root == nullptr) {
             return 0;
         }
 
-        int const leftDepth = 1 + maxDepth(root->left);
-        int const rightDepth = 1 + maxDepth(root->right);
+        int result = 0;
 
-        return std::max(leftDepth, rightDepth);
+        // Create a queue to store nodes from root
+        // and push the root node into the queue
+        queue<TreeNode*> q;
+        q.push(root);
+
+        // Exit the while loop when the queue is empty
+        while (!q.empty()) {
+            const int size = static_cast<int>(q.size());
+
+            // Iterate through every nodes in the queue,
+            // which is the nodes in a level
+            for (int i = 0; i < size; ++i) {
+                // Get the node from the queue,
+                // which is the very left node for this level
+                // if this is the first iteration of the for loop
+                const TreeNode* node = q.front();
+                q.pop();
+
+                // Push that node's left child into the queue if it exists
+                if (node->left != nullptr) {
+                    q.push(node->left);
+                }
+
+                // Push that node's right child into the queue if it exists
+                if (node->right != nullptr) {
+                    q.push(node->right);
+                }
+            }
+            // Increment the result as we finished a level
+            result++;
+        }
+        return result;
     }
 };
