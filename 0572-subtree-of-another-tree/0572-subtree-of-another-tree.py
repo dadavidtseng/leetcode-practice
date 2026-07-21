a99854eadaaea634e9e83e1b171d88fa778b344e
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-07-21 16:50:53 -0500

    Time: 49 ms (15.94%), Space: 20.8 MB (5%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -6,6 +6,19 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def isSameTree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+            # Return true if both nodes are empty
+            if p is None and q is None:
+                return True
+
+            # If both nodes aren't empty and they have the same value,
+            # push their left/right nodes into recursive call
+            if p is not None and q is not None and p.val == q.val:
+                return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+
+            # Return false if none of the above checks match
+            return False
+
         # Return true if root and subRoot are both empty
         if root is None and subRoot is None:
             return True
@@ -14,59 +27,10 @@ class Solution:
         if root is None or subRoot is None:
             return False
 
-        def serialize(node: Optional[TreeNode]) -> str:
-            # Return "$#" if this node is empty
-            if node is None:
-                return "$#"
+        # Return true if root and subRoot are the same tree
+        if isSameTree(root, subRoot):
+            return True
 
-            # Return the serialized tree string after pushing node's left/right nodes into DFS
-            return "$" + str(node.val) + serialize(node.left) + serialize(node.right)
-
-        def z_function(s: str) -> List[int]:
-            # 1. z[i] = length of the longest common prefix between s[i...] and s[0...]
-            # 2. Maintain the rightmost Z-box [L, R), where s[L...R-1] == s[0...(R-L)-1]
-            s_size = len(s)
-            z = [0] * s_size
-            L = 0
-            R = 0
-
-            # Compute z[i] for every suffix starting at i
-            for i in range(1, s_size):
-                # If i falls inside the current Z-box, we can reuse the previously computed
-                # value at i - L. However, the match cannot extend beyond the current Z-box,
-                # so we cap it at R - i.
-                if i < R:
-                    z[i] = min(R - i, z[i - L])
-
-                # Try to extend the current match beyond the Z-box
-                # z[i] is the current matched prefix length
-                while i + z[i] < s_size and s[z[i]] == s[i + z[i]]:
-                    z[i] += 1
-
-                # If we extended farther than the current Z-box,
-                # update it to the new rightmost Z-box
-                if i + z[i] > R:
-                    L = i
-                    R = i + z[i]
-            return z
-
-        # 1. Serialize text from root
-        # 2. Serialize pattern from subRoot
-        # 3. Combine pattern, delimeter, and text
-        # Z-function compares every suffix with the prefix of the combined string.
-        # Therefore, we put pattern at the beginning so that z[i] tells us how much
-        # the current suffix matches the pattern.
-        text = serialize(root)
-        pattern = serialize(subRoot)
-        combined = pattern + "|" + text
-
-        pattern_length = len(pattern)
-        combined_length = len(combined)
-        z = z_function(combined)
-
-        # From patternLength + 1 to the end,
-        # if z[i] equals patternLength, that means pattern is in text
-        for i in range(pattern_length + 1, combined_length):
-            if z[i] == pattern_length:
-                return True
-        return False
+        # Push in root's left/right nodes into two recursive calls if root and subRoot are not the
+        # same tree
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
