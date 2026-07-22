83b9d23e589e6b3a99aaa8facddda7342875e45a
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-07-22 18:24:36 -0500

    Time: 119 ms (5.04%), Space: 20.2 MB (72.63%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -3,13 +3,10 @@ class Solution:
         result = [0] * (n + 1)
 
         # Iterate through 1 to n
-        for i in range(1, n + 1):
-            num = i
-
-            # Exit the while loop when n isn't 0
-            while num != 0:
-                # Flip the rightmost 1 bit to 0
-                # Note that n | (n + 1) can flip the rightmost 0 bit to 1
-                num = num & (num - 1)
-                result[i] += 1
+        for num in range(1, n + 1):
+            # Iterate through all 32 bits in num
+            for i in range(32):
+                # Increment result[num] if ith bit in num is 1
+                if num & (1 << i):
+                    result[num] += 1
         return result
