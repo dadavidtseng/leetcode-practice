7587b26b0e5b564b2ed5517c5136a00fb33f5e36
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-07-22 15:35:44 -0500

    Time: 2 ms (38.58%), Space: 63 MB (11.21%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int const size = static_cast<int>(height.size());
+        const int heightSize = static_cast<int>(height.size());
         int result = 0;
-        int left = 0;
-        int right = size - 1;
+        int L = 0;
+        int R = heightSize - 1;
 
-        while (left < right) {
-            int area = 0;
+        // Exit the while loop when left/right pointers cross
+        // 1. Pick whichever is smaller between height[L] and height[R] as H
+        // 2. Calculate W
+        // 3. Assign the maximum between result and area(W*H) to result
+        while (L < R) {
+            int H = 0;
+            const int W = R - L;
 
-            if (height[left] < height[right]) {
-                area = (right - left) * height[left];
-                left++;
-            } else if (height[left] > height[right]) {
-                area = (right - left) * height[right];
-                right--;
+            if (height[L] < height[R]) {
+                H = height[L];
+                L++;
+            } else if (height[L] > height[R]) {
+                H = height[R];
+                R--;
             } else {
-                area = (right - left) * height[left];
-                left++;
-                right--;
+                H = height[L];
+                L++;
+                R--;
             }
-
-            result = max(result, area);
+            result = max(result, H * W);
         }
-
         return result;
     }
 };
