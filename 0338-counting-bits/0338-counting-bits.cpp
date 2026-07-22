2a34fcb1a21a86c6c5f3aec72cc24bfdc1b41d0b
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-07-22 18:26:01 -0500

    Time: 0 ms (100%), Space: 10.6 MB (57.04%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -5,15 +5,7 @@ public:
 
         // Iterate through 1 to n
         for (int i = 1; i <= n; ++i) {
-            int num = i;
-
-            // Exit the while loop when n isn't 0
-            while (num != 0) {
-                // Flip the rightmost 1 bit to 0
-                // Note that n | (n + 1) can flip the rightmost 0 bit to 1
-                num = num & (num - 1);
-                result[i]++;
-            }
+            result[i] = popcount(static_cast<unsigned int>(i));
         }
         return result;
     }
