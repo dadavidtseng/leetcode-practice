7f86839516684c14cc6314ff6a219d0d8160b8bc
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-07-22 18:24:56 -0500

    Time: 0 ms (100%), Space: 20.2 MB (43.2%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -3,10 +3,6 @@ class Solution:
         result = [0] * (n + 1)
 
         # Iterate through 1 to n
-        for num in range(1, n + 1):
-            # Iterate through all 32 bits in num
-            for i in range(32):
-                # Increment result[num] if ith bit in num is 1
-                if num & (1 << i):
-                    result[num] += 1
+        for i in range(1, n + 1):
+            result[i] = i.bit_count()
         return result
