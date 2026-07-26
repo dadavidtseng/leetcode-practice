13b190e9d376405502ed59802dc7495bb1c99dac
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-07-26 14:33:27 -0500

    Time: 4 ms (4.64%), Space: 24.7 MB (7.66%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -13,40 +13,23 @@
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
-        // 1. current node is root
-        // 2. a stack for LIFO
-        TreeNode* cur = root;
-        stack<TreeNode*> s;
+        vector<int> result;
 
-        // exit the loop when current node is nullptr and the stack is empty
-        while (cur != nullptr || !s.empty()) {
-            // exit the loop when current node is nullptr,
-            // otherwise, push the current node into stack
-            // and keep going left
-            while (cur != nullptr) {
-                s.push(cur);
-                cur = cur->left;
+        // Brutally traverse through the BST and store every node's values in
+        // result
+        auto dfs = [&](this auto&& self, TreeNode* node) -> void {
+            if (node == nullptr) {
+                return;
             }
+            result.push_back(node->val);
+            self(node->left);
+            self(node->right);
+        };
 
-            // 1. set current node to the top of the stack
-            // this is essentially visiting that node
-            // 2. remove the visited node since we don't need it anymore
-            // 3. decrease k by 1
-            cur = s.top();
-            s.pop();
-            k--;
+        dfs(root);
 
-            // if k is 0, that mean we've reached our goal, return the current
-            // node's value
-            if (k == 0) {
-                return cur->val;
-            }
-
-            // move the current node to right
-            cur = cur->right;
-        }
-
-        // we'll never reach this because 1 <= k <= n
-        return 0;
+        // Sort result and return kth smallest element
+        sort(result.begin(), result.end());
+        return result[k - 1];
     }
 };
