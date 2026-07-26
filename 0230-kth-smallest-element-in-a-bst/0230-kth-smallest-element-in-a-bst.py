6a537579631c3b4c7bb0fbd7f7f33fdedd8d1f79
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-07-26 14:32:01 -0500

    Time: 3 ms (29.94%), Space: 22.3 MB (23.21%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -8,16 +8,16 @@ class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         result = []
 
-        # Brutally traverse through the BST and store every node's values in result
+        # Use inorder traversal for BST and push node's value into result as it goes
+        # Note that result will be sorted naturally because of the nature of BST
         def dfs(node: Optional[TreeNode]) -> None:
             if node is None:
                 return
-            result.append(node.val)
             dfs(node.left)
+            result.append(node.val)
             dfs(node.right)
 
         dfs(root)
 
-        # Sort result and return kth smallest element
-        result.sort()
+        # Return kth smallest element
         return result[k - 1]
