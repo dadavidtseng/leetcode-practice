d020f2e43879a9b91b5e0d128810e0f7bfb6696a
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-07-29 15:53:29 -0500

    Time: 0 ms (100%), Space: 10.3 MB (9.5%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int L = 0;
-        int R = static_cast<int>(matrix.size()) - 1;
+        const int matrix_size = static_cast<int>(matrix.size());
 
-        while (L < R) {
-            for (int i = 0; i < (R - L); ++i) {
-                int top = L;
-                int bottom = R;
+        // Reverse the matrix horizontally
+        reverse(matrix.begin(), matrix.end());
 
-                // Cache the top left
-                int topLeft = matrix[top][L + i];
-
-                // Move bottom left to top left
-                matrix[top][L + i] = matrix[bottom - i][L];
-
-                // Move bottom right to bottom left
-                matrix[bottom - i][L] = matrix[bottom][R - i];
-
-                // Move top right to bottom right
-                matrix[bottom][R - i] = matrix[top + i][R];
-
-                // Move top left to top right
-                matrix[top + i][R] = topLeft;
+        // Transpose the matrix
+        for (int i = 0; i < matrix_size; ++i) {
+            for (int j = i + 1; j < matrix_size; ++j) {
+                swap(matrix[i][j], matrix[j][i]);
             }
-            // Move left/right pointers to inner layer
-            R--;
-            L++;
         }
     }
 };
