d501550157dc04de8b99f27b21ba3c5e1bbae019
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-07-29 15:48:36 -0500

    Time: 0 ms (100%), Space: 19.1 MB (92.63%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,11 +1,28 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        matrix_size = len(matrix)
-        rotated = [[0] * matrix_size for _ in range(matrix_size)]
+        L = 0
+        R = len(matrix) - 1
 
-        for i in range(matrix_size):
-            for j in range(matrix_size):
-                # 1. m[i][j] = m[j][i] -> transpose
-                # 2. m[j][(n - 1) - i] -> mirror by x-axis
-                rotated[j][(matrix_size - 1) - i] = matrix[i][j]
-        matrix[:] = rotated
+        while L < R:
+            for i in range(R - L):
+                top = L
+                bottom = R
+
+                # Cache the top left
+                top_left = matrix[top][L + i]
+
+                # Move bottom left to top left
+                matrix[top][L + i] = matrix[bottom - i][L]
+
+                # Move bottom right to bottom left
+                matrix[bottom - i][L] = matrix[bottom][R - i]
+
+                # Move top right to bottom right
+                matrix[bottom][R - i] = matrix[top + i][R]
+
+                # Move top left to top right
+                matrix[top + i][R] = top_left
+
+            # Move left/right pointers to inner layer
+            L += 1
+            R -= 1
