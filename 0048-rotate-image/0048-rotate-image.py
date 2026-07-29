4b7721d262725e56d5dd2006b5b88e631ee38c43
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-07-29 15:48:09 -0500

    Time: 0 ms (100%), Space: 19.4 MB (27.67%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,11 +1,11 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         matrix_size = len(matrix)
+        rotated = [[0] * matrix_size for _ in range(matrix_size)]
 
-        # Reverse the matrix horizontally
-        matrix.reverse()
-
-        # Transpose the matrix
         for i in range(matrix_size):
-            for j in range(i + 1, matrix_size):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            for j in range(matrix_size):
+                # 1. m[i][j] = m[j][i] -> transpose
+                # 2. m[j][(n - 1) - i] -> mirror by x-axis
+                rotated[j][(matrix_size - 1) - i] = matrix[i][j]
+        matrix[:] = rotated
