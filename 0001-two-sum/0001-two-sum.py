a29ab5b2616f660799568cac8ea174c6fd1a0328
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-07-31 16:25:10 -0500

    Time: 4 ms (42.2%), Space: 20.4 MB (40.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_size = len(nums)
+        num_index_map = {}
 
-        # Iterate through every possible pairs in nums
-        for i in range(nums_size):
-            for j in range(i + 1, nums_size):
-                # return result if we've found target
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        # Iterate through every element to find target
+        # Build num_index_map along the way
+        for i in range(len(nums)):
+            diff = target - nums[i]
 
+            if diff in num_index_map:
+                return [num_index_map[diff], i]
+            num_index_map[nums[i]] = i
         return []
