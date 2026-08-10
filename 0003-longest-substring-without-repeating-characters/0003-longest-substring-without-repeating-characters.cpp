a66134c87f8d6b0a79deae991784137049e87ca7
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-10 16:33:42 -0500

    Time: 51 ms (18.14%), Space: 19.4 MB (14.99%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<char> charSet;
+        // Create a map to store char and index
+        unordered_map<char, int> m;
         int L = 0;
         int result = 0;
 
-        // Extend right pointer if we haven't seen that character before
+        // Extend right pointer and store it in the map
+        // 1. Move left pointer over right pointer only if we've seen it before
+        // 2. Store the index into right pointer's character in the map
+        // 3. Update result to keep track of longest window(substring)
         for (int R = 0; R < static_cast<int>(s.size()); ++R) {
-            // Move left pointer until charSet doesn't contain s[R]
-            while (charSet.contains(s[R])) {
-                charSet.erase(s[L]);
-                L++;
+            char const c = s[R];
+
+            if (m.contains(c)) {
+                L = max(L, m[c] + 1);
             }
-            charSet.insert(s[R]);
+            m[c] = R;
             result = max(result, R - L + 1);
         }
         return result;
