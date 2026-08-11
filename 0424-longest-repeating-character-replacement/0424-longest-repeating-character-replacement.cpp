fffa2778ea013ea2848ddee651f9243f12a495e2
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-11 18:07:15 -0500

    Time: 12 ms (23.44%), Space: 10.9 MB (42.04%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -15,15 +15,14 @@ public:
             // update the maxFrequency when window[c] updates
             maxFrequency = max(maxFrequency, window[c]);
 
-            // while window length(i-left+1) minus maxFrequency in window is
-            // greater than k, means that this window is invalid
+            // while window length(i-left+1) minus maxFrequency in window is greater than k,
+            // means that this window is invalid
             while ((i - left + 1) - maxFrequency > k) {
                 window[s[left]]--; // remove the left char in window
                 left++;            // move left pointer to right
             }
 
-            // update the result if window length is greater than previous
-            // result
+            // update the result if window length is greater than previous result
             result = max(result, i - left + 1);
         }
 
