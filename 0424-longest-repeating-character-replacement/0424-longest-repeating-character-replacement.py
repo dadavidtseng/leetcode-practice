93176e3cf94da0c999eb2814d548a1633bfac133
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-12 14:05:47 -0500

    Time: 77 ms (80.65%), Space: 19.7 MB (50.88%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,26 +1,24 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        char_set = set(s)
+        freq = {}
+        L = 0
+        max_freq = 0
         result = 0
 
-        # For each distinguish character, maintain a sliding window to find out the max result
-        for c in char_set:
-            # count is count of c inside the window
-            # L is the left pointer of the window
-            count = 0
-            L = 0
+        # Extend the window by moving its right pointer
+        # and record the max window size (R-L+1) as we go
+        for R in range(len(s)):
+            c = s[R]
 
-            # Extend the window by moving its right pointer
-            # and record the max window size (R-L+1) as we go
-            for R in range(len(s)):
-                if s[R] == c:
-                    count += 1
+            # Use a frequency map to keep track of the maxFreq for each distinguish character
+            # Note that maxFreq may sometimes be inaccurate because we don't decrement it
+            # We don't decrement maxFreq because we want to get the maximum window size
+            freq[c] = freq.get(c, 0) + 1
+            max_freq = max(max_freq, freq[c])
 
-                # Shrink the window by moving its left pointer when that window is invalid
-                # Decrement count if we encounter c when shrinking the window
-                while (R - L + 1) - count > k:
-                    if s[L] == c:
-                        count -= 1
-                    L += 1
-                result = max(result, R - L + 1)
+            # Remove the leftmost character from the frequency map as we shrink the window
+            while (R - L + 1) - max_freq > k:
+                freq[s[L]] -= 1
+                L += 1
+            result = max(result, R - L + 1)
         return result
