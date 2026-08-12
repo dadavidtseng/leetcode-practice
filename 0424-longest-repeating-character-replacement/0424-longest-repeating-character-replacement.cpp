9229be5dcc8710ccd6deedf3d51a7dbad64ef7ae
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-12 12:00:55 -0500

    Time: 8 ms (37.24%), Space: 11 MB (42.04%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        unordered_map<char, int> window; // stores char and frequency
-        int left = 0;
+        unordered_set<char> charSet(s.begin(), s.end());
         int result = 0;
-        int maxFrequency = 0;
 
-        for (int i = 0; i < static_cast<int>(s.size()); ++i) {
-            char const c = s[i];
+        // For each distinguish character, maintain a sliding window to find out
+        // the max result
+        for (char c : charSet) {
+            // count is count of c inside the window
+            // L is the left pointer of the window
+            int count = 0;
+            int L = 0;
 
-            // add the frequency of c in window
-            window[c]++;
+            // Extend the window by moving its right pointer
+            // and record the max substringLength as we go
+            for (int R = 0; R < static_cast<int>(s.length()); ++R) {
+                if (s[R] == c) {
+                    count++;
+                }
 
-            // update the maxFrequency when window[c] updates
-            maxFrequency = max(maxFrequency, window[c]);
-
-            // while window length(i-left+1) minus maxFrequency in window is greater than k,
-            // means that this window is invalid
-            while ((i - left + 1) - maxFrequency > k) {
-                window[s[left]]--; // remove the left char in window
-                left++;            // move left pointer to right
+                // Shrink the window by moving its left pointer when that window
+                // is invalid Decrement count if we encounter c when shrinking
+                // the window
+                while ((R - L + 1) - count > k) {
+                    if (s[L] == c) {
+                        count--;
+                    }
+                    L++;
+                }
+                result = max(result, R - L + 1);
             }
-
-            // update the result if window length is greater than previous result
-            result = max(result, i - left + 1);
         }
-
         return result;
     }
 };
