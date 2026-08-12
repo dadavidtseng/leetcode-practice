62c4d3912548e4fdd123cf39a616879c1f977a71
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-12 13:42:30 -0500

    Time: 6 ms (47.23%), Space: 11 MB (42.04%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        unordered_set<char> charSet(s.begin(), s.end());
+        unordered_map<char, int> freq; // stores char and frequency
+        int L = 0;
+        int maxFreq = 0;
         int result = 0;
 
-        // For each distinguish character, maintain a sliding window to find out
-        // the max result
-        for (char c : charSet) {
-            // count is count of c inside the window
-            // L is the left pointer of the window
-            int count = 0;
-            int L = 0;
+        // Extend the window by moving its right pointer
+        // and record the max window size (R-L+1) as we go
+        for (int R = 0; R < static_cast<int>(s.length()); ++R) {
+            const char c = s[R];
 
-            // Extend the window by moving its right pointer
-            // and record the max substringLength as we go
-            for (int R = 0; R < static_cast<int>(s.length()); ++R) {
-                if (s[R] == c) {
-                    count++;
-                }
+            // Use a frequency map to keep track of the maxFreq for each
+            // distinguish character Note that maxFreq may sometimes be
+            // inaccurate because we don't decrement it We don't decrement
+            // maxFreq because we want to get the maximum window size
+            freq[c]++;
+            maxFreq = max(maxFreq, freq[c]);
 
-                // Shrink the window by moving its left pointer when that window
-                // is invalid Decrement count if we encounter c when shrinking
-                // the window
-                while ((R - L + 1) - count > k) {
-                    if (s[L] == c) {
-                        count--;
-                    }
-                    L++;
-                }
-                result = max(result, R - L + 1);
+            // Remove the leftmost character from the frequency map as we shrink
+            // the window
+            while ((R - L + 1) - maxFreq > k) {
+                freq[s[L]]--;
+                L++;
             }
+            result = max(result, R - L + 1);
         }
         return result;
     }
