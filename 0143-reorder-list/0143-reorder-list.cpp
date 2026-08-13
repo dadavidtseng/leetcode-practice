e0db0f98e240eb718d3efff9694541cf86e0d623
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-13 14:25:22 -0500

    Time: 0 ms (100%), Space: 24 MB (6.35%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -11,58 +11,33 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        // find the middle node in the list by using slow/fast pointer
-        ListNode* slow = head;
-        ListNode const* fast = head;
-
-        // advance slow/fast pointer when
-        // 1. fast pointer is not nullptr(even list)
-        // 2. fast pointer's next is not nullptr(odd list)
-        while (fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;
-            fast = fast->next->next;
+        // Return early if provided head is empty
+        if (head == nullptr) {
+            return;
         }
 
-        // split the list in half
-        // 1. attach original head to first
-        // 2. attach slow pointer's next to second(this works both on odd/even
-        // list)
-        // 3. cut the second half from first half(original head)
-        ListNode* first = head;
-        ListNode* second = slow->next;
-        slow->next = nullptr;
+        // Create an empty container to store ListNode*
+        // and attach head to curr
+        vector<ListNode*> nodes;
+        ListNode* curr = head;
 
-        // reverse the second list
-        // 1. store the next node in temp
-        // 2. set the next node to pre
-        // 3. advance the pre node
-        // 4. advance the cur node
-        // attach pre to the head of second half(cur is nullptr now)
-        ListNode* pre = nullptr;
-        ListNode* cur = second;
-
-        while (cur != nullptr) {
-            ListNode* temp = cur->next;
-            cur->next = pre;
-            pre = cur;
-            cur = temp;
+        // Push every nodes into container by going through the linkedlist
+        while (curr != nullptr) {
+            nodes.push_back(curr);
+            curr = curr->next;
         }
 
-        second = pre;
+        int L = 0;
+        int R = static_cast<int>(nodes.size()) - 1;
 
-        // iterate the first haft and insert the second half one after another
-        // in first half exit the loop when second is nullptr we don't need to
-        // check first because for first half is one node more than second half
-        // in odd list
-        while (second != nullptr) {
-            ListNode* tempFirst = first->next;
-            ListNode* tempSecond = second->next;
-
-            first->next = second;
-            second->next = tempFirst;
-
-            first = tempFirst;
-            second = tempSecond;
+        // Reorder nodes using left/right pointers
+        while (L < R) {
+            nodes[L]->next = nodes[R];
+            L++;
+            nodes[R]->next = nodes[L];
+            R--;
         }
+        // Set the tail's next pointer points to prevent a cycle
+        nodes[L]->next = nullptr;
     }
 };
