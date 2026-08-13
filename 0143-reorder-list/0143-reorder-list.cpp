e06a7e781d8aca54fc2f61f0b3aafb5c4d01d36d
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-13 15:53:04 -0500

    Time: 0 ms (100%), Space: 22.8 MB (53.34%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -11,38 +11,61 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        // Return early if provided head is empty or there's only one node in
-        // the linkedlist
-        if (head == nullptr || head->next == nullptr) {
-            return;
+        // Find the middle node in linkedlist using slow/fast pointers
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        // Advance slow/fast pointers when
+        // 1. Fast pointer is not nullptr(even list)
+        // 2. Fast pointer's next is not nullptr(odd list)
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        auto dfs = [&](this auto&& self, ListNode*& front,
-                       ListNode* back) -> bool {
-            // Return true if back is empty
-            if (back == nullptr) {
-                return true;
-            }
 
-            if (!self(front, back->next)) {
-                // Note that this could return whatever because it's just for
-                // existing the DFS call
-                return false;
-            }
+        // Split the list in half
+        // 1. Attach original head to first
+        // 2. Attach slow pointer's next to second(this works both on odd/even
+        // list)
+        // 3. Cut the second half from first half(original head)
+        ListNode* first = head;
+        ListNode* second = slow->next;
+        slow->next = nullptr;
 
-            // Set the new tail (back->next) to empty and return false
-            if (front == back || front->next == back) {
-                back->next = nullptr;
-                return false;
-            }
+        // Reverse the second list
+        // 1. Store the next node in temp
+        // 2. Set the next node to prev
+        // 3. Advance the prev node
+        // 4. Advance the curr node
+        // Attach prev to the head of second half(cur is nullptr now)
+        ListNode* prev = nullptr;
+        ListNode* curr = second;
 
-            // Reorder nodes using front/back pointers
-            ListNode* temp = front->next;
-            front->next = back;
-            back->next = temp;
-            front = temp;
+        while (curr != nullptr) {
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
 
-            return true;
-        };
-        dfs(head, head->next);
+        second = prev;
+
+        // Iterate through the first haft and insert the second half one after
+        // another in first half exit the loop when second is empty
+        //
+        // Note that we don't need to check first because for first half is one
+        // node more than second half in odd list
+        while (second != nullptr) {
+            ListNode* tempFirst = first->next;
+            ListNode* tempSecond = second->next;
+
+            // Reorder nodes
+            first->next = second;
+            second->next = tempFirst;
+
+            // Advance first and second for next iteration
+            first = tempFirst;
+            second = tempSecond;
+        }
     }
 };
