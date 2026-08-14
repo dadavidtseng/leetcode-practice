c4f3534aefa21b1a5393ea1760d46fb10e0bdd13
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-14 11:44:43 -0500

    Time: 0 ms (100%), Space: 19.3 MB (64.15%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,24 +5,28 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        # Create an empty container to store node
-        # and attach head to curr
-        nodes = []
+        nodes_size = 0
         curr = head
 
-        # Push every nodes into container by going through the linkedlist
+        # Calculate the number of nodes in the linkedlist by iterating through it
         while curr:
-            nodes.append(curr)
+            nodes_size += 1
             curr = curr.next
-
-        remove_idx = len(nodes) - n
+        remove_idx = nodes_size - n
 
         # Return early without doing the removal
         if remove_idx == 0:
             return head.next
 
-        # Remove the target node from the linkedlist
-        nodes[remove_idx - 1].next = nodes[remove_idx].next
+        # Move current node back to head for next iteration
+        curr = head
+
+        # Iterate through the linkedlist again and break after removing the target node
+        for i in range(nodes_size):
+            if (i + 1) == remove_idx:
+                curr.next = curr.next.next
+                break
+            curr = curr.next
 
         # Return the modified linkedlist's head
         return head
