5e7f079691f73005aef533e981de8c20129415b8
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-16 16:37:30 -0500

    Time: 70 ms (26.44%), Space: 22.9 MB (18.26%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -10,24 +10,19 @@ class Solution:
     def lowestCommonAncestor(
         self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
     ) -> "TreeNode":
+        def dfs(node: "TreeNode") -> "TreeNode":
+            # If current node's value is greater than p's and q's value, go left
+            if node.val > p.val and node.val > q.val:
+                return dfs(node.left)
+
+            # If current node's value is less than p's and q's value, go right
+            if node.val < p.val and node.val < q.val:
+                return dfs(node.right)
+
+            # Return the lowest common ancestor
+            return node
+
         # Return empty if any of root, p, and q is empty
         if not root or not p or not q:
             return None
-
-        curr = root
-
-        while curr:
-            # If current node's value is greater than p's and q's value, go left
-            if curr.val > p.val and curr.val > q.val:
-                curr = curr.left
-
-            # If current node's value is less than p's and q's value, go right
-            elif curr.val < p.val and curr.val < q.val:
-                curr = curr.right
-
-            # Return the lowest common ancestor
-            else:
-                return curr
-
-        # This will never be reached because we've already checked if root/p/q are valid
-        return None
+        return dfs(root)
