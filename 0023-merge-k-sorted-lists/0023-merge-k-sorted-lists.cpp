1d7b148e9ec19b85148da1b8df09a9309d88f4f0
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-16 12:58:24 -0500

    Time: 7 ms (26.95%), Space: 19.2 MB (9.27%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -11,71 +11,31 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int n = static_cast<int>(lists.size());
+        vector<int> nodes;
 
-        // return nullptr if the lists is empty
-        if (n == 0)
-            return nullptr;
-
-        // exit when n == 1
-        while (n > 1) {
-            // reset newSize after a merge
-            int newSize = 0;
-
-            // iterate the lists with 2 steps
-            for (int i = 0; i < n; i += 2) {
-                // odd lists, store the last list back to lists
-                if (i + 1 == n) {
-                    lists[newSize] = lists[i];
-                } else {
-                    // merge two lists and store back to lists[newSize]
-                    lists[newSize] = mergeTwoLists(lists[i], lists[i + 1]);
-                }
-
-                // calculate newSize
-                // lists will be shrunk after every operation
-                newSize++;
+        // Iterate through lists and push every list into nodes
+        for (int i = 0; i < static_cast<int>(lists.size()); ++i) {
+            while (lists[i] != nullptr) {
+                nodes.push_back((lists[i]->val));
+                lists[i] = lists[i]->next;
             }
-
-            // update the n with newSize
-            // this will shrink size of the lists
-            n = newSize;
         }
 
-        // return the merged list
-        return lists[0];
-    }
+        // Sort every node, which creates O(n log n) for time complexity
+        sort(nodes.begin(), nodes.end());
 
-    ListNode* mergeTwoLists(ListNode* listA, ListNode* listB) {
-        // initialize a dummy node and attach a tail node on that dummy node
-        ListNode dummy(0, nullptr);
-        ListNode* tail = &dummy;
+        ListNode* result = new ListNode(0);
+        ListNode* curr = result;
 
-        // exit the loop when listA and listB are both exhausted
-        while (listA != nullptr && listB != nullptr) {
-            // when listA's value is smaller than listB's value
-            // 1. attach listA to tail
-            // 2. advance listA
-            if (listA->val < listB->val) {
-                tail->next = listA;
-                listA = listA->next;
-            }
-            // when listA's value is larger or equal to listB's value
-            // 1. attach listB to tail
-            // 2. advance listB
-            else {
-                tail->next = listB;
-                listB = listB->next;
-            }
-
-            // advance tail
-            tail = tail->next;
+        // Iterate through nodes,
+        // 1. Create new node using nodes[i]
+        // 2. Advance current node
+        for (int i = 0; i < static_cast<int>(nodes.size()); ++i) {
+            curr->next = new ListNode(nodes[i]);
+            curr = curr->next;
         }
 
-        // attach the remaining of listA and listB
-        tail->next = (listA != nullptr) ? listA : listB;
-
-        // return the head of the merged list
-        return dummy.next;
+        // Return result's next node because result is a dummy node
+        return result->next;
     }
 };
