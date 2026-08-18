2e49dbd46ed2b5a269b765c89d50c054807c6c82
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-18 13:24:39 -0500

    Time: 454 ms (12.99%), Space: 34.2 MB (5.39%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.py
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -4,6 +4,7 @@ class Solution:
 
         def dfs(is_alice: bool, i: int, M: int) -> int:
             piles_size = len(piles)
+
             if i == piles_size:
                 return 0
             if (is_alice, i, M) in dp:
@@ -13,9 +14,10 @@ class Solution:
             total = 0
 
             for X in range(1, 2 * M + 1):
-                if X + i > len(piles):
+                if X + i > piles_size:
                     break
                 total += piles[X + i - 1]
+
                 if is_alice:
                     result = max(result, total + dfs(not is_alice, X + i, max(M, X)))
                 else:
