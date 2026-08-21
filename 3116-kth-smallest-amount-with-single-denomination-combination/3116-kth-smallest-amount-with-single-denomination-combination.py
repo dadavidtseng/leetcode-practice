5d1987533954f3f8af04ade445ee2aa5df60c6fc
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-21 14:19:12 -0500

    Time: 176 ms (18.29%), Space: 20.8 MB (21.95%) - LeetHub

--- 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.py
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.py
@@ -32,44 +32,78 @@ Note that,
 
 class Solution:
     def findKthSmallest(self, coins: List[int], k: int) -> int:
-        def lcm(a, b):
+        def lcm(a, b):  # O(1) because 1 <= coins[i] <= 25
             x, y = a, b
 
+            # Calculate greatest common divisor(gcd)
             while y:
                 x, y = y, x % y
+
+            # Return least common multiple(lcm),
+            # Note that we use a // x * b because (a * b // x)'s (a * b) might overflow
             return a // x * b
 
         L = min(coins)  # x_min
         R = min(coins) * k  # x_max
 
+        # Precompute all possible subsets in coins
         subsets = []
 
-        for mask in range(1, 1 << len(coins)):
+        # Iterate through every masks
+        # Note that we can also use 2 ** len(coins) here
+        # because left shift 1 bit means multiply by 2 once.
+        for mask in range(1, 1 << len(coins)):  # O(2^n - 1)
             subset_lcm = 1
             subset_size = 0
 
-            for i in range(len(coins)):
+            # Iterate through every coin
+            for i in range(len(coins)):  # O(n)
+                # If current coin is selected by current mask
+                # 1. Increment subset_size
+                # 2. Calculate current subset's lcm
                 if mask & (1 << i):
                     subset_size += 1
                     subset_lcm = lcm(subset_lcm, coins[i])
 
+                    # Break if we're outside the range
+                    # and there's no need to keep calculating subset_lcm
                     if subset_lcm > R:
                         break
+            # After calculating subset_lcm for current mask,
+            # if we're still inside the range,
+            # push (subset_lcm, sign) into subsets array
             if subset_lcm <= R:
                 sign = 1 if subset_size % 2 else -1
                 subsets.append((subset_lcm, sign))
 
+        # x is the amount of coins we want to make,
+        # return the distinguish amount using every coins.
+        # Ex. x=10, coins=[2,5], return= 10//2 + 10//5 - 10//1cm(2,5)
         def count(x: int) -> int:
             result = 0
 
-            for subset_lcm, sign in subsets:
+            for subset_lcm, sign in subsets:  # O(2^n - 1)
                 result += sign * (x // subset_lcm)
             return result
 
-        while L < R:
+        # Perform a binary search to find the exact k we can produce using every coins
+        while L < R:  # O(log R) = O(log (min(coins) * k))
             M = L + (R - L) // 2
-            if count(M) >= k:
-                R = M
-            else:
+            if count(M) < k:
                 L = M + 1
+            else:
+                R = M
         return L
+
+
+"""
+# Review
+I was able to come up with the naive solution using set and the concept of binary search. It was fun solving and learning this problem, but I don't think I can come up with the solution in an interview without hints because it covers a lot of concepts to solve.
+
+# Evaluate
+I guess my focus should be on medium problem and occationally do hard problem. When solving hard problem, it can easily review concepts in wider rages.
+
+# Complexity
+Time: O(2^n * (n + log(min(coins) * k)))
+Space: O(2^n)
+"""
