fc175791418e1a19e70cd31043a206081f44808a
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-22 07:32:15 -0500

    Time: 0 ms (100%), Space: 19.3 MB (58.35%) - LeetHub

---
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.py
@@ -0,0 +1,49 @@
+"""
+Understand
+Given a integer n with a range of 1 <= n <= 10^6,
+return true if n is divisible by the sum of,
+1. digit sum of n
+2. digit product of n
+
+Match
+Math
+
+Plan
+Extract n's digit and compute the sum and product of the digit
+return true if n % (sum + product) == 0
+"""
+
+
+class Solution:
+    def checkDivisibility(self, n: int) -> bool:
+        num = n
+        total = 0
+        product = 1
+
+        # Use a while loop
+        while n:
+            # n % 10 to extract the last digit
+            digit = n % 10
+
+            # n // 10 to remove the last digit
+            n //= 10
+
+            # Compute the sum and product of all digits
+            total += digit
+            product *= digit
+
+        # Return true if n % (sum + product) == 0
+        return num % (total + product) == 0
+
+
+"""
+Review
+The core concept of this solution is integer manipulation using `%` and `//`
+
+Evaluate
+There's also other ways to do this question, such as string conversion and mapping from string to integer.
+
+Complexity:
+Time: O(log10(n)), the value of n scales by a factor of 10 for each digit added.
+Space: O(1)
+"""
