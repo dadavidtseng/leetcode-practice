bc82c79bf7fcbf7570d3e076b0ea2b56cb148e3c
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-22 15:54:17 -0500

    Time: 0 ms (100%), Space: 20.2 MB (99.47%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,37 +6,55 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        preorder_size = len(preorder)
+        # Create preIdx and inIdx to keep track of where we are in preorder and inorder
         pre_idx = 0
         in_idx = 0
 
-        def dfs(limit: Optional[int]) -> Optional[TreeNode]:
-            nonlocal pre_idx
-            nonlocal in_idx
+        # Note that we only care about preoder's size because we are building the tree using
+        # preorder as boundary. For inorder, we use it to know whether we should build left/right
+        # subtrees and that has nothing to do with its size.
+        preorder_size = len(preorder)
 
-            # Return empty if we've processed all elements in preorder
-            if pre_idx >= preorder_size:
-                return None
+        dummy = TreeNode(0, None)
+        curr = dummy
 
-            # If there's a limit and the value of that limit equals current element in inorder
-            # 1. Increment inorder's index
-            # 2. Return empty
-            if limit is not None and limit == inorder[in_idx]:
-                in_idx += 1
-                return None
-
-            # Get root node's value from preorder[preorder's index]
-            # and then increment preorder's index
-            root_val = preorder[pre_idx]
+        # Exit the while loop when eith preIdx or inIdx equal or greater than preorder's size
+        while pre_idx < preorder_size and in_idx < preorder_size:
+            # 1. Create a new node and have this new node points to current node's right
+            # 2. Assign current node's right to the new node(create/pass the thread!)
+            # 3. Move current node to its right
+            # 4. Increment preorder index
+            new_node = TreeNode(preorder[pre_idx], None, curr.right)
+            curr.right = new_node
+            curr = curr.right
             pre_idx += 1
 
-            # Create root node
-            root = TreeNode(root_val)
+            # Exit the while loop when current node's value is inorder[inIdx]
+            # because that's the end of left subtree
+            # 1. Create a new node and have this new node points to current node
+            # 2. Assign current node's left to the new node
+            # 3. Move current node to its left
+            # 4. Increment preorder index
+            while curr.val != inorder[in_idx]:
+                new_node = TreeNode(preorder[pre_idx], None, curr)
+                curr.left = new_node
+                curr = curr.left
+                pre_idx += 1
 
-            # Pass in new limit for left/right subtrees for DFS call
-            root.left = dfs(root_val)
-            root.right = dfs(limit)
-            return root
+            # Increment inorder index so that inorder[inIdx] points to root node's value
+            in_idx += 1
 
-        # Start the DFS call with no limit
-        return dfs(None)
+            # Exit the while loop when current node's right is empty or current node's right vaule
+            # isn't root node in that subtree.
+            # 1. Cache current node's right as root node using the thread we created when building
+            # left subtree
+            # 2. Remove the thread by assigning current node's right to empty
+            # 3. Move current node back to root node(parent)
+            # 4. Increment inorder index
+            while curr.right and curr.right.val == inorder[in_idx]:
+                root = curr.right
+                curr.right = None
+                curr = root
+                in_idx += 1
+
+        return dummy.right
