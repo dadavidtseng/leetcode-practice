a799cffdfda529d28c1870eee746d020120bcdcf
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-27 16:07:39 -0500

    Time: 7 ms (86.84%), Space: 19.4 MB (44.74%) - LeetHub

---
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
@@ -0,0 +1,62 @@
+class Solution:
+    def lexGreaterPermutation(self, s: str, target: str) -> str:
+        freq = [0] * 26
+        result = ""
+
+        for c in s:
+            freq[ord(c) - ord("a")] += 1
+
+        for i in range(len(target)):
+            idx = ord(target[i]) - ord("a")
+
+            if freq[idx] > 0:
+                result += target[i]
+                freq[idx] -= 1
+            else:
+                while idx < 25 and freq[idx + 1] <= 0:
+                    idx += 1
+
+                if idx < 25 and freq[idx + 1] > 0:
+                    result += chr(idx + 1 + ord("a"))
+                    freq[idx + 1] -= 1
+
+                    for j in range(26):
+                        result += chr(j + ord("a")) * freq[j]
+
+                    return result
+
+                # Backtrack
+                for j in range(i - 1, -1, -1):
+                    prev_idx = ord(target[j]) - ord("a")
+                    freq[prev_idx] += 1
+                    result = result[:-1]
+
+                    for k in range(prev_idx + 1, 26):
+                        if freq[k] > 0:
+                            result += chr(k + ord("a"))
+                            freq[k] -= 1
+
+                            for m in range(26):
+                                result += chr(m + ord("a")) * freq[m]
+
+                            return result
+
+                return ""
+
+        # Backtrack if the entire target was matched
+        for j in range(len(target) - 1, -1, -1):
+            prev_idx = ord(target[j]) - ord("a")
+            freq[prev_idx] += 1
+            result = result[:-1]
+
+            for k in range(prev_idx + 1, 26):
+                if freq[k] > 0:
+                    result += chr(k + ord("a"))
+                    freq[k] -= 1
+
+                    for m in range(26):
+                        result += chr(m + ord("a")) * freq[m]
+
+                    return result
+
+        return ""
