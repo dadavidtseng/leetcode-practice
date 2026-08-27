fa006c08ad07891458813ee6e39061c1241f5432
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-27 16:17:32 -0500

    Time: 3 ms (97.37%), Space: 19.6 MB (21.05%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
@@ -1,7 +1,7 @@
 class Solution:
     def lexGreaterPermutation(self, s: str, target: str) -> str:
         freq = [0] * 26
-        result = ""
+        result = []
 
         for c in s:
             freq[ord(c) - ord("a")] += 1
@@ -10,36 +10,36 @@ class Solution:
             idx = ord(target[i]) - ord("a")
 
             if freq[idx] > 0:
-                result += target[i]
+                result.append(target[i])
                 freq[idx] -= 1
             else:
                 while idx < 25 and freq[idx + 1] <= 0:
                     idx += 1
 
                 if idx < 25 and freq[idx + 1] > 0:
-                    result += chr(idx + 1 + ord("a"))
+                    result.append(chr(idx + 1 + ord("a")))
                     freq[idx + 1] -= 1
 
                     for j in range(26):
-                        result += chr(j + ord("a")) * freq[j]
+                        result.append(chr(j + ord("a")) * freq[j])
 
-                    return result
+                    return "".join(result)
 
                 # Backtrack
                 for j in range(i - 1, -1, -1):
                     prev_idx = ord(target[j]) - ord("a")
                     freq[prev_idx] += 1
-                    result = result[:-1]
+                    result.pop()
 
                     for k in range(prev_idx + 1, 26):
                         if freq[k] > 0:
-                            result += chr(k + ord("a"))
+                            result.append(chr(k + ord("a")))
                             freq[k] -= 1
 
                             for m in range(26):
-                                result += chr(m + ord("a")) * freq[m]
+                                result.append(chr(m + ord("a")) * freq[m])
 
-                            return result
+                            return "".join(result)
 
                 return ""
 
@@ -47,16 +47,16 @@ class Solution:
         for j in range(len(target) - 1, -1, -1):
             prev_idx = ord(target[j]) - ord("a")
             freq[prev_idx] += 1
-            result = result[:-1]
+            result.pop()
 
             for k in range(prev_idx + 1, 26):
                 if freq[k] > 0:
-                    result += chr(k + ord("a"))
+                    result.append(chr(k + ord("a")))
                     freq[k] -= 1
 
                     for m in range(26):
-                        result += chr(m + ord("a")) * freq[m]
+                        result.append(chr(m + ord("a")) * freq[m])
 
-                    return result
+                    return "".join(result)
 
         return ""
