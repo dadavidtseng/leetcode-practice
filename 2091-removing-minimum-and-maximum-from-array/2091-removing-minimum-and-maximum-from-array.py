2b17ec92ba426c9c68a4ec1d5298d0a2b7920934
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-30 15:08:28 -0500

    Time: 37 ms (11.24%), Space: 34.1 MB (11.24%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -1,27 +1,27 @@
 class Solution:
     def minimumDeletions(self, nums: List[int]) -> int:
         nums_size = len(nums)
+        min_idx = 0
+        max_idx = 0
 
-        if nums_size == 1:
-            return 1
-
-        seen_min = float("inf")
-        seen_max = float("-inf")
-        seen_min_idx = 0
-        seen_max_idx = 0
-
+        # Iterate through nums to get min_idx and max_idx
         for i in range(nums_size):
-            if nums[i] < seen_min:
-                seen_min = nums[i]
-                seen_min_idx = i
-            if nums[i] > seen_max:
-                seen_max = nums[i]
-                seen_max_idx = i
+            if nums[i] < nums[min_idx]:
+                min_idx = i
+            if nums[i] > nums[max_idx]:
+                max_idx = i
 
-        L = min(seen_min_idx, seen_max_idx)
-        R = max(seen_min_idx, seen_max_idx)
+        # Compute L and R from min_idx and max_idx
+        L = min(min_idx, max_idx)
+        R = max(min_idx, max_idx)
+
+        # There will be three possibilities,
+        # 1. Both on left side
+        # 2. Both on right side
+        # 3. L on left side and R on right side
         LL = R + 1
         RR = nums_size - L
         LR = (L + 1) + (nums_size - R)
 
+        # Return the mininum of all possibilities
         return min(LL, RR, LR)
