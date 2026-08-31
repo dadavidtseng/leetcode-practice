c794d6919dd8f56e55d46da6a5802db8ff0743da
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-08-31 12:36:35 -0500

    Time: 101 ms (22.42%), Space: 63 MB (57.18%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -5,30 +5,29 @@
 #         self.next = next
 class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
-        if not head:
+        if not head or not head.next or not head.next.next:
             return [-1, -1]
+
+        def is_critical(x, y, z) -> bool:
+            return (y.val - x.val) * (y.val - z.val) > 0
 
         # Keep track of minimum/maximum distance between two critical points
         min_dist = float("inf")
 
-        curr = head
-        prev = curr
-        curr = curr.next
+        prev, curr, next = head, head.next, head.next.next
         curr_idx = 1
         first_idx = -1
         last_idx = -1
 
         # Iterate through the linkedlist
-        while curr and curr.next:
-            if (prev.val > curr.val < curr.next.val) or (
-                prev.val < curr.val > curr.next.val
-            ):
+        while next:
+            if is_critical(prev, curr, next):
                 if last_idx != -1:
                     min_dist = min(min_dist, curr_idx - last_idx)
                 if first_idx == -1:
                     first_idx = curr_idx
                 last_idx = curr_idx
             curr_idx += 1
-            prev = curr
-            curr = curr.next
+            prev, curr, next = curr, next, next.next
+
         return [-1, -1] if first_idx == last_idx else [min_dist, last_idx - first_idx]
