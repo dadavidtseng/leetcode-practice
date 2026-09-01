863775e6d76c4cdbfa67908f3d78f229e2c2930e
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-09-01 08:38:07 -0500

    Time: 1561 ms (89.47%), Space: 24.7 MB (91.23%) - LeetHub

--- 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.py
+++ 3568-minimum-moves-to-clean-the-classroom/3568-minimum-moves-to-clean-the-classroom.py
@@ -12,49 +12,49 @@ class Solution:
                     sx, sy = (row, col)
                 elif classroom[row][col] == "L":
                     L_idx[(row, col)] = L_count
-
                     L_count += 1
 
         best_e = [[[-1] * (1 << L_count) for _ in range(n)] for _ in range(m)]
         best_e[sx][sy][0] = energy
         q = deque([(sx, sy, 0, energy, 0)])
         full_mask = (1 << L_count) - 1
-        full_energy = energy
+        full_e = energy
 
         while q:
-            R, C, mask, e, steps = q.popleft()
+            r, c, mask, e, moves = q.popleft()
 
             if mask == full_mask:
-                return steps
+                return moves
 
             if e == 0:
                 continue
 
             directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-            for r, c in directions:
-                new_r = R + r
-                new_c = C + c
+            for dr, dc in directions:
+                new_r = r + dr
+                new_c = c + dc
 
                 if new_r < 0 or new_c < 0 or new_r >= m or new_c >= n:
                     continue
 
-                if classroom[new_r][new_c] == "X":
+                grid = classroom[new_r][new_c]
+
+                if grid == "X":
                     continue
 
                 new_e = e - 1
                 new_mask = mask
 
-                if classroom[new_r][new_c] == "L":
-                    new_mask |= (1 << L_idx[(new_r, new_c)])
+                if grid == "L":
+                    new_mask |= 1 << L_idx[(new_r, new_c)]
 
-                if classroom[new_r][new_c] == "R":
-                    new_e = full_energy
+                if grid == "R":
+                    new_e = full_e
 
                 if best_e[new_r][new_c][new_mask] >= new_e:
                     continue
 
                 best_e[new_r][new_c][new_mask] = new_e
-
-                q.append((new_r, new_c, new_mask, new_e, steps + 1))
+                q.append((new_r, new_c, new_mask, new_e, moves + 1))
         return -1
