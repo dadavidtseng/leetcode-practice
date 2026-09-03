f723a1d72c5bdf322003705771fa51d19283f172
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-09-03 11:13:28 -0500

    Time: 74 ms (50%), Space: 35.2 MB (73.17%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -4,35 +4,21 @@ class Solution:
             return True
 
         min_odd = float("inf")
-        min_even = float("inf")
-
-        # Iterate through nums to compute min_odd and min_even
+        
+        # Iterate through nums to compute min_odd
         for n in nums:
-            # odd
             if n % 2:
                 min_odd = min(min_odd, n)
-            # even
-            else:
-                min_even = min(min_even, n)
 
-        can_odd = True
-        can_even = True
-        print(min_odd, min_even)
+        # All even
+        if min_odd == float("inf"):
+            return True      
+        
         # Try all odd
         for n in nums:
-            if not n % 2:
-                if n - min_odd < 1:
-                    print("Try all odd")
-                    can_odd = False
-                    break
+            # even
+            if not n % 2 and n - min_odd < 1:
+                return False
 
-        # Try all even
-        for n in nums:
-            if n % 2:
-                if n - min_odd < 1:
-                    print("Try all even")
-                    can_even = False
-                    break
-        print(can_odd, can_even)
-        return can_odd or can_even
+        return True
             
