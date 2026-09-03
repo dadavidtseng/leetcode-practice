c47bcd0582b2afa2c7ea9dbb0cb87e69087f4bde
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-09-03 11:17:12 -0500

    Time: 65 ms (56.1%), Space: 35.5 MB (18.9%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -1,24 +1,26 @@
 class Solution:
     def uniformArray(self, nums: list[int]) -> bool:
+        # Return true if there's only one element in nums
         if len(nums) == 1:
             return True
 
         min_odd = float("inf")
-        
+
         # Iterate through nums to compute min_odd
         for n in nums:
             if n % 2:
                 min_odd = min(min_odd, n)
 
-        # All even
+        # odd  + odd  = even
+        # odd  + even = odd
+        # even + even = even
+
+        # Return true if all elements are even
         if min_odd == float("inf"):
-            return True      
-        
-        # Try all odd
+            return True
+
+        # Iterate through nums to find even and see if we can turn it into odd
         for n in nums:
-            # even
             if not n % 2 and n - min_odd < 1:
                 return False
-
         return True
-            
