c82c2acc3a7d5ba410f7700b12862277cab1c88b
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-09-07 19:49:57 -0500

    Time: 7 ms (89.9%), Space: 19.4 MB (44.95%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.py
@@ -1,15 +1,13 @@
 class Solution:
     def distinctSubseqII(self, s: str) -> int:
+        MOD = 10**9 + 7
         n = len(s)
         dp = [1] * (n + 1)
         last = [-1] * 26
 
         for i in range(n - 1, -1, -1):
             idx = ord(s[i]) - ord("a")
-            dp[i] = 2 * dp[i + 1]
-            if last[idx] == -1:
-                dp[i] = 2 * dp[i + 1]
-            else:
-                dp[i] = 2 * dp[i + 1] - dp[last[idx] + 1]
+            duplicated = 0 if last[idx] == -1 else dp[last[idx] + 1]
+            dp[i] = (2 * dp[i + 1] - duplicated) % MOD
             last[idx] = i
-        return (dp[0] - 1)% (10**9 + 7)
+        return (dp[0] - 1) % MOD
