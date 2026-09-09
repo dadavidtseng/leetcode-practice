e43f47a013ebd7083ed8487d43d0891c7be857ff
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-09-09 14:46:25 -0500

    Time: 0 ms (100%), Space: 19.2 MB (47.32%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
@@ -1,23 +1,9 @@
 class Solution:
     def countCommas(self, n: int) -> int:
         result = 0
-        num = n
-        digit = 0
-        while num != 0:
-            num //= 10
-            digit += 1
+        p = 1000
         
-        if digit < 4:
-            result = 0
-        elif 3 <digit < 7:
-            result = n - 999
-        elif 6 <digit < 10:
-            result = (n - 999999) * 2 + 999000
-        elif 9 <digit < 13:
-            result = (n - 999999999) * 3 + 999000000 * 2 + 999000
-        elif 12 <digit < 16:
-            result = (n - 999999999999) * 4 + 999000000000 * 3 + 999000000 * 2 + 999000
-        else:
-            result = (n - 999999999999) * 4 + 999000000000 * 3 + 999000000 * 2 + 999000 + 1
-
+        while p <= n:
+            result += n - p + 1
+            p *= 1000
         return result
