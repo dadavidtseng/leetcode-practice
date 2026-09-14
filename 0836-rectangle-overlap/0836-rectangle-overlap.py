f45664f10ecc6294e73e2b58c9b8d29717fc5a7b
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-09-13 23:32:15 -0500

    Time: 0 ms (100%), Space: 19.5 MB (19.53%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.py
+++ 0836-rectangle-overlap/0836-rectangle-overlap.py
@@ -5,8 +5,9 @@ class Solution:
         # bottom-right [2][1]
         # top-left     [0][3]
         # top-right    [2][3]
-        if rec1[2] <= rec2[0] or rec2[2] <= rec1[0]:
-            return False
-        if rec1[3] <= rec2[1] or rec2[3] <= rec1[1]:
-            return False
-        return True
+        return (
+            rec1[2] > rec2[0]
+            and rec2[2] > rec1[0]
+            and rec1[3] > rec2[1]
+            and rec2[3] > rec1[1]
+        )
